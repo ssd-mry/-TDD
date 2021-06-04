2e404bca67186b1113c0ce661eed422ef209abe4
ssd-mry <1483211934@qq.com>
2021-06-04 13:57:55 +0800

    first strps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT=10
 
 class NewVisitorTest(LiveServerTestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
@@ -92,34 +93,40 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox=self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		# 2    time.sleep(1)
-
-		# rows=table.find_elements_by_tag_name('tr')
-		# self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-		# self.assertIn(
-		# 	'2:Use peacock feathers to make a fly',
-		# 	[row.text for row in rows]
-		# 	)
-		# self.assertTrue(
-		# 	any(row.text=='1:Buy peacock feathers' for row in rows),
-		# 	f"New to-do item did not appear in table.Contents were\n{table.text}"
-		# 	)
-		#  3     
+		
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
-		# self.fail('Finish the test!')
 
-# The page updates again, and now shows both items on her list
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
 
-# Edith wonders whether the site will remember her list. The she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
 
-# She visits that URL - her to-do list is still there.
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
 
+
+
+
+      
 # Satisfied, she goes back to sleep
 if __name__ == '__main__':
 	unittest.main(warnings = 'ignore')
